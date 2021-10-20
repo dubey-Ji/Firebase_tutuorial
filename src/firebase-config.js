c3fa3279494a792459b9928229e68694cf002951
dubey-Ji <ashd1014@hotmail.com>
2021-10-20 20:05:31 +0530

    Firebase Connected

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: REACT_APP_APIKEY,
+  authDomain: REACT_APP_AUTHDOMAIN,
+  projectId: REACT_APP_PROJECTID,
+  storageBucket: REACT_APP_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_MESSAGINGSENDERID,
+  appId: REACT_APP_APPID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
